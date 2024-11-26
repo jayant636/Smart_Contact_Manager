48b25807b3292d6026d954435026c10ac8ff089b
jayant636 <jayantjalandra@gmail.com>
2024-11-26 21:17:43 +0530

    Basic Page layout + Route setup done

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,37 @@ public class PageController {
         model.addAttribute("title","No 1 Coder");
 
         return "Home";
+    }
+
+    @RequestMapping(path = "/about")
+    public String aboutRoute(Model model){
+        model.addAttribute("name","Substring TXhnologies");
+        model.addAttribute("title","No 1 Coder");
+
+        return "About";
+    }
+
+    @RequestMapping(path = "/service")
+    public String serviceRoute(Model model){
+        model.addAttribute("name","Substring TXhnologies");
+        model.addAttribute("title","No 1 Coder");
+
+        return "Services";
+    }
+
+    @GetMapping(path = "/contact")
+    public String contact(){
+        return "Contact";
+    }
+
+    @GetMapping(path = "/login")
+    public String login(){
+        return "Login";
+    }
+
+    @GetMapping(path = "/register")
+    public String signup(){
+        return "Register";
     }
 
 }
